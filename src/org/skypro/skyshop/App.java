bd7d27907a792e9180a5b3fcac97299a6fdd7cfa
MariaSizz <buchacik@mail.ru>
2025-07-16 00:28:51 +0300

    HW is done.

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -14,18 +14,17 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        // Создание товаров
+
         Product phone = new SimpleProduct("Телефон", 15000);
         Product laptop = new DiscountProduct("Ноутбук", 50000, 10);
         Product headphones = new FixPriceProduct("Наушники");
         System.out.println(phone);
-        // Создание корзины
+
         ProductBasket basket = new ProductBasket();
         basket.addProduct(phone);
         basket.addProduct(laptop);
         basket.addProduct(headphones);
 
-        //удаления существующего продукта
         System.out.println("Удаление 'Телефон':");
         List<Product> removedPhones = basket.removeByName("Телефон");
         System.out.println("Удалено продуктов: " + removedPhones.size());
@@ -35,7 +34,6 @@ public class App {
         System.out.println("\nСодержимое корзины:");
         basket.printBasketContents();
 
-        // удаления несуществующего продукта
         System.out.println("\nУдаление 'Монитор':");
         List<Product> removedUnknown = basket.removeByName("Монитор");
         if (removedUnknown.isEmpty()) {
@@ -44,14 +42,12 @@ public class App {
         System.out.println("\nСодержимое корзины:");
         basket.printBasketContents();
 
-        // Создание SearchEngine
         SearchEngine searchEngine = new SearchEngine();
         searchEngine.add(phone);
         searchEngine.add(laptop);
         searchEngine.add(headphones);
         searchEngine.add(new Article("Обзор телефона", "Телефон имеет отличную камеру."));
 
-        // поиск
         System.out.println("\nПоиск по 'телефон':");
         List<Searchable> results = searchEngine.search("телефон");
         for (Searchable item : results) {
